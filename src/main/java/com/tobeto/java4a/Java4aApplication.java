61a92afed04cfb96fec8eaf5e26a81ce6c6a340b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-19 14:47:51 +0300

    init

---
+++ src/main/java/com/tobeto/java4a/Java4aApplication.java
@@ -0,0 +1,13 @@
+package com.tobeto.java4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java4aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobeto/java4a/Java4aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeto.java4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
