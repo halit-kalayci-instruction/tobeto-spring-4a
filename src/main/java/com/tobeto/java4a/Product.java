51ad9d8d3954df43c65f0fb2f4676d31943079ad
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-19 15:03:58 +0300

    controllers

---
+++ src/main/java/com/tobeto/java4a/Product.java
@@ -0,0 +1,12 @@
+package com.tobeto.java4a;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private int id;
+    private String name;
+    private double unitPrice;
+}
---
+++ src/main/java/com/tobeto/java4a/ProductsController.java
@@ -0,0 +1,30 @@
+package com.tobeto.java4a;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController
+{
+    @GetMapping
+    public String sayHello(@RequestParam String name)
+    {
+        // DB'e bağlanıp
+        // veri ekle. veri sorgula. veri güncelle
+        return "Merhaba " + name;
+    }
+
+    @PostMapping
+    public Product sayGoodBye(@RequestBody Product product)
+    {
+        return product;
+    }
+    // Web, HTTP, HTTP Anatomy
+    // Medium
+
+    @PutMapping
+    public String saySomething()
+    {
+        return "Bla bla..";
+    }
+}
